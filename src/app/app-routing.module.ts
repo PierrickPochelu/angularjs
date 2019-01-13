e749499b6806f445d7bfb4911a530c3166042eb4
Pierrick Pochelu <pierrick.pochelu@gmail.com>
2019-01-13 20:49:52 +0100

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CatsComponent} from './cats/cats.component';
+import {DogsComponent} from './dogs/dogs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'cats',component:CatsComponent},
+{path:'dogs',component:DogsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PdfViewerComponent } from 'ng2-pdf-viewer';
+import { FormsModule } from '@angular/forms';
+
+import { HeroesComponent } from './heroes/heroes.component';
+import { CatsComponent } from './cats/cats.component';
+import { DogsComponent } from './dogs/dogs.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PdfViewerComponent,
+
+    HeroesComponent,
+    CatsComponent,
+    DogsComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
